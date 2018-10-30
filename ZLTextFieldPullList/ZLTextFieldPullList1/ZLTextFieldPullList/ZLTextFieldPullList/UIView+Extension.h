2e7b4bd82e3fe5cfc157fbb18853a1ba3ebb8358
zhul <zhul@zhulindeMacBook-Pro.local>
2018-10-30 16:02:00 +0800

    add project

---
+++ ZLTextFieldPullList/ZLTextFieldPullList1/ZLTextFieldPullList/ZLTextFieldPullList/UIView+Extension.h
@@ -0,0 +1,19 @@
+//
+//  UIView+Extension.h
+//  ZLTextFieldPullList
+//
+//  Created by zhul on 2018/10/29.
+//  Copyright © 2018 cn.com.csii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+@property(nonatomic,assign,readonly) CGFloat x;
+@property(nonatomic,assign,readonly) CGFloat y;
+@property(nonatomic,assign,readonly) CGFloat width;
+@property(nonatomic,assign,readonly) CGFloat height;
+
+@property(nonatomic,assign,readonly) CGFloat tail;
+@property(nonatomic,assign,readonly) CGFloat bottom;
+@end
